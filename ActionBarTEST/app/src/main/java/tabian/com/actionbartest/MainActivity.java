8b2a5971d947b01a185e8c599bb50a596348a920
Mitch <mitch@tabian.ca>
2017-04-12 06:48:30 -0700

    Source Code

---
+++ ActionBarTEST/app/src/main/java/tabian/com/actionbartest/MainActivity.java
@@ -0,0 +1,41 @@
+package tabian.com.actionbartest;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_assignment);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_autorenew);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_attach_file);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment());
+        adapter.addFragment(new Tab2Fragment());
+        adapter.addFragment(new Tab3Fragment());
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ ActionBarTEST/app/src/main/java/tabian/com/actionbartest/SectionsPagerAdapter.java
@@ -0,0 +1,36 @@
+package tabian.com.actionbartest;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by User on 4/11/2017.
+ */
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter{
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment){
+        mFragmentList.add(fragment);
+    }
+}
